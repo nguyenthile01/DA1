9ec6fea92dcd7aea538542c8850d733d9e14dea4
vodat12 <vothanhdat112@gmail.com>
2019-11-17 16:59:17 +0700

    add FE

---
+++ FindJob/coreui-free-angular-admin-template-master/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Cities } from './city.model';
+import { DataService } from './data.service';
+
+@Component({
+  // tslint:disable-next-line
+  selector: 'body',
+  template: '<router-outlet></router-outlet>'
+})
+export class AppComponent implements OnInit {
+  cities: Cities[];
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+  }
+}
+
+
