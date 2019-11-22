5aca779e46da2124c7793e57f036559979ed77ee
nguyenthile <lent9497@gmail.com>
2019-11-22 21:26:28 +0700

    update

---
+++ Web/src/app/_nav.ts
@@ -0,0 +1,96 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    title: true,
+    name: 'Danh mục'
+  },
+  {
+    name: 'Quản lý user',
+    url: '/base',
+    icon: 'icon-puzzle',
+    children: [
+      {
+        name: 'Cards',
+        url: '/base/cards',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Carousels',
+        url: '/base/paginations',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Collapses',
+        url: '/base/collapses',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Forms',
+        url: '/base/forms',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Table',
+        url: '/base/tables',
+        icon: 'icon-puzzle'
+      },
+    ]
+  },
+  {
+    name: 'Quản lý emloyer',
+    url: '/buttons',
+    icon: 'icon-cursor',
+    children: [
+      {
+        name: 'Buttons',
+        url: '/buttons/buttons',
+        icon: 'icon-cursor'
+      },
+      {
+        name: 'Dropdowns',
+        url: '/buttons/dropdowns',
+        icon: 'icon-cursor'
+      },
+      {
+        name: 'Brand Buttons',
+        url: '/buttons/brand-buttons',
+        icon: 'icon-cursor'
+      }
+    ]
+  },
+  {
+    name: 'Hồ sơ ứng tuyển',
+    url: '/charts',
+    icon: 'icon-pie-chart',
+    children: [
+      {
+        name: 'Buttons',
+        url: '/buttons/buttons',
+        icon: 'icon-cursor'
+      },
+      {
+        name: 'Dropdowns',
+        url: '/buttons/dropdowns',
+        icon: 'icon-cursor'
+      },
+      {
+        name: 'Brand Buttons',
+        url: '/buttons/brand-buttons',
+        icon: 'icon-cursor'
+      }
+    ]
+  },
+  {
+    divider: true
+  },
+];
